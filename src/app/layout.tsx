9290dc24c528916cdd918bd5541d5aa4550c50b3
Anju1023 <niafuguapricottaro@gmail.com>
2025-08-14 12:45:56 +0900

    レイアウトファイルにコメントを追加し、フォント設定を削除しました。また、投稿データの配列を新規作成し、記事情報を管理する関数を実装しました。

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
+// 画面の共通レイアウト。ヘッダー/フッターなどをここに入れる
+// 子ページは{children}に差し込まれる！
+
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/layouts/header/Header';
 import Footer from '@/components/layouts/footer/Footer';
-
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
 	title: 'ゆるまる暮しの学びログ',
